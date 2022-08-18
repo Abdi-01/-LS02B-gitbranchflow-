bbde329bdd5fa40908cd20128f5bac24fe254bb8
ariqr11 <ariqr.11@gmail.com>
2022-08-18 14:05:22 +0700

    LSN-02

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import HotNews from './Components/HotNews';
+import TopStories from './Components/TopStories';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  return (<div>
+    <HotNews />
+    <TopStories />
+  </div>
   );
 }
 
